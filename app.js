77676661ca218e1b30b009338236f9e21e361d9f
DamonRod <damonrod2004@outlook.com>
2023-12-06 21:38:30 -0500

    Updated pug templates and css

--- app.js
+++ app.js
@@ -85,4 +85,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
 module.exports = app;
