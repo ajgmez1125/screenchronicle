468fc1438c735b6a0afc52cdc46d3e596651cd71
ajgmez1125 <ajgg1125@gmail.com>
2023-12-09 23:21:34 -0500

    watchlist functionality pretty much done

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Media = require('../models/media')
+const User = require('../models/user')
 const passport = require('passport')
 
 router.get('/', function(req, res, next) {
@@ -15,5 +16,39 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'ScreenChronicle', error: 'There was an error loading media'})
   })
 });
+
+router.get('/dashboard', (req,res) => {
+  User.findById(req.user._id)
+  .then((user) => {
+    var context = {
+      watched: [],
+      watching: [],
+      plantowatch: []
+    }
+
+    promises = []
+
+    promises.push(Media.find({_id: {$in: user.watched}})
+    .then((docs) => {
+      console.log(docs)
+      context.watched = docs
+    }))
+
+    promises.push(Media.find({_id: {$in: user.watching}})
+    .then((docs) => {
+      context.watching = docs
+    }))
+
+    promises.push(Media.find({_id: {$in: user.plantowatch}})
+    .then((docs) => {
+      context.plantowatch = docs
+    }))
+
+    Promise.all(promises)
+    .then(() => {
+      res.render('dashboard', context)
+    })
+  })
+})
 
 module.exports = router;
--- routes/movie.js
+++ routes/movie.js
@@ -2,16 +2,33 @@ var express = require('express');
 var router = express.Router();
 var Media = require('../models/media')
 var mongoose = require('mongoose')
-var User = require('../models/user')
+var User = require('../models/user');
+const user = require('../models/user');
 
 
 router.get('/:id', (req, res) => {
     Media.findOne({_id: req.params.id})
     .then((media) => {
-        res.render('media', {media: media})
+        let status = null
+        if(req.isAuthenticated())
+        {
+            if(req.user.watched.includes(req.params.id))
+            {
+                status = 'watched'
+            }
+            else if(req.user.watching.includes(req.params.id))
+            {
+                status = 'watching'
+            }
+            else if(req.user.plantowatch.includes(req.params.id))
+            {
+                status = 'plantowatch'
+            }
+        }
+        res.render('media', {media: media, status: status})
     })
     .catch((err) => {
-        res.send('Could not find movie')
+        res.send(err.message)
     })
 });
 
@@ -25,13 +42,46 @@ router.get('/:id/add/:progress', (req,res) => {
                 progress = req.params.progress.toLowerCase()
                 Media.findById(req.params.id)
                 .then((movie) => {
-                    user[progress] = [...user[progress], movie] 
-                    user.save()
-                    res.send('Success')
+                    if(!user[progress].includes(req.params.id))
+                    {
+                        if(progress !== 'watched' && user['watched'].includes(req.params.id))
+                        {
+                            let newArr = user['watched']
+                            newArr = user['watched'].filter((movie) => {
+                                return !movie._id.equals(req.params.id)
+                            })
+                            user['watched'] = newArr
+                        }
+
+                        if(progress !== 'watching' && user['watching'].includes(req.params.id))
+                        {
+                            let newArr = user['watching']
+                            newArr = user['watching'].filter((movie) => {
+                                return !movie._id.equals(req.params.id)
+                            })
+                            user['watching'] = newArr
+                        }
+
+                        if(progress !== 'plantowatch' && user['plantowatch'].includes(req.params.id))
+                        {
+                            let newArr = user['plantowatch']
+                            newArr = user['plantowatch'].filter((movie) => {
+                                return !movie._id.equals(req.params.id)
+                            })
+                            user['plantowatch'] = newArr
+                        }
+                        user[progress] = [...user[progress], movie] 
+                        user.save()
+                        res.redirect(`/media/${req.params.id}`)
+                    }
+                    else
+                    {
+                        res.send(movie.title + ` has already been added to ` + progress)
+                    }
+                    
                 })
                 .catch((err) => {
-                    console.log(JSON.stringify(err))
-                    res.send('Could not get movie')
+                    res.send(err.message)
                 })
             }
         })
@@ -45,8 +95,54 @@ router.get('/:id/add/:progress', (req,res) => {
     }
 })
 
-router.get('/:id/remove/:progress', () => {
+router.get('/:id/remove/', (req,res) => {
+    if(req.isAuthenticated())
+    {
+        User.findById(req.user._id)
+        .then((user) => {
+            Media.findById(req.params.id)
+            .then((movie) => {
+                if(user['watched'].includes(req.params.id))
+                {
+                    let newArr = user['watched']
+                    newArr = user['watched'].filter((movie) => {
+                        return !movie._id.equals(req.params.id)
+                    })
+                    user['watched'] = newArr
+                }
 
+                if(user['watching'].includes(req.params.id))
+                {
+                    let newArr = user['watching']
+                    newArr = user['watching'].filter((movie) => {
+                        return !movie._id.equals(req.params.id)
+                    })
+                    user['watching'] = newArr
+                }
+
+                if(user['plantowatch'].includes(req.params.id))
+                {
+                    let newArr = user['plantowatch']
+                    newArr = user['plantowatch'].filter((movie) => {
+                        return !movie._id.equals(req.params.id)
+                    })
+                    user['plantowatch'] = newArr
+                }
+                user.save()
+                res.redirect(`/media/${req.params.id}`)
+            })
+            .catch((err) => {
+                res.send(err.message)
+            })
+        })
+        .catch((err) => {
+            res.send(err.message)
+        })
+    }
+    else
+    {
+        res.send('You are not logged in')
+    }
 })
 
 module.exports = router;
