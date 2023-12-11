ba9bfe16e1c1e15e9c8f1dee7a1773083c2088d4
RYAN MORRIS <ryanmorris1414@gmail.com>
2023-12-11 12:58:33 -0500

    branchpushmonday

--- routes/movie.js
+++ routes/movie.js
@@ -3,17 +3,58 @@ var router = express.Router();
 var Media = require('../models/media')
 var mongoose = require('mongoose')
 var User = require('../models/user')
+var Review = require('../models/review')
 
 
 router.get('/:id', (req, res) => {
     Media.findOne({_id: req.params.id})
     .then((media) => {
-        res.render('media', {media: media})
+        res.render('media', {media: media, reviews: media.reviews});
+
+        Review.find({mediaId: mediaId })
+            .then((reviews)=>{
+                res.render('media',{media:media,reviews:reviews});
+            })
+            .catch((err)=>{
+                res.send('broke');
+            })
     })
     .catch((err) => {
         res.send('Could not find movie')
     })
 });
+
+
+router.post('/:id/addReview', (req,res)=>{
+    if(req.isAuthenticated()){
+        const userId = req.user._id;
+        const mediaId = req.params.id;
+        const { reviewString, rating } = req.body;
+
+        const newReview = new Review({
+            user: userId,
+            reviewString: reviewString,
+            rating: rating,
+            mediaId: mediaId
+        });
+
+        newReview.save()
+            .then((review)=>{
+                Media.findByIdAndUpdate(mediaId, {$push: {reviews:review._id}})
+                    .then(()=>{
+                        res.redirect(`/media/${mediaId}`);
+                    })
+                    .catch((err)=>{
+                        res.send('Error associating review with media');
+                    });
+            })
+            .catch((err)=>{
+                res.send('Error saving Review');
+            });
+    }else{
+        res.send('You are not logged in');
+    }
+})
 
 router.get('/:id/add/:progress', (req,res) => {
     if(req.isAuthenticated())
